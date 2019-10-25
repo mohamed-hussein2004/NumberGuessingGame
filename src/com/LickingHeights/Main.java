6a2cdff130647aa698d561f1451eb3755f1b4e12
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-10-25 09:33:17 -0400

    added the do while loop

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,11 +1,36 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	int randomNummber;
+	int guess;
+		Scanner  keyboard;
+		keyboard = new Scanner(System.in);
+	String correct;
 
+		System.out.println("Guess the number i am thinking of 1 - 100");
+		guess = keyboard.nextInt();
 	randomNummber = (int) (Math.random()*100)+1;
 	System.out.println(randomNummber);
-    }
-}
+	do{
+		if (guess > randomNummber) {
+			System.out.println("your to high");
+		}
+		else if (guess < randomNummber) {
+			System.out.println("your to low");
+		}
+
+
+
+	}while (guess != randomNummber);
+
+
+
+
+	}
+
+   }
+
